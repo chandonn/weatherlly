0fe127879c65bfb43ac9ff30659d9f40e2f545da
Alexandre <alexandrecorreadeoliveira@gmail.com>
2024-01-23 17:41:20 -0300

    Update

--- src/components/Forecast/ForecastDay.tsx
+++ src/components/Forecast/ForecastDay.tsx
@@ -1,17 +1,14 @@
-import React, { useContext, useState } from "react"
+import { useContext } from "react"
 import { Weather } from "../../types/data"
 import { WeatherIcon } from "../Icon/Icon"
 import { Store } from "../../store/state"
 
 export const ForecastDay = (props: Weather) => {
-    const [hover, setHover] = useState(false)
     const { dispatchUpdateBackgroundConfig } = useContext(Store)
-    
+
     return (
         <div
             className="forecast-day"
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}
             onClick={() => dispatchUpdateBackgroundConfig(props.description)}
         >
             <div>
